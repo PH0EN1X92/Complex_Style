b333434e83dde0f6c54f416ad1e70b9e8cb99f35
Ilyushin Kirill <netrunner093@gmail.com>
2023-12-27 15:04:58 +0500

    code was repaired

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include     "mycomplex.h"
 using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
+int main()
+{
+  //РёРЅРёС†РёР°Р»РёР·Р°С†РёСЏ 
+   Complex A,B(-4), C(23.0, 45.9);
+  
+  //РІС‹РІРѕРґ
+   cout<<A <<","<< B<<","<<C <<endl;
+  
+  //СЃСѓРјРјР°
+    A=B+C;
+    cout<<"A=B+C,A="<< A<<endl;
 
+  //РІС‹С‡РёС‚Р°РЅРёРµ
+    Complex M=B-C;
+    cout<< "M = B - C, M = " << M << endl;
+    cout<<"M*A="<<M*A<<endl;
+    cout<<"M/4.45="<<M/4.45<<endl;
+
+  //РІС‹РІРѕРґ РєРѕРјРїР»РµРєСЃРЅРѕРіРѕ Р”
     Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+    cout<<"Enter complex number D = "; cin >>D;
+
+  //РґРѕРї РѕРїРµСЂР°С†РёРё
+    A+=C+D;
+    cout << "D = " << D << "\nA = " << A << endl;
+    
+    return 0;
+}
